f959bdda8551b35df4f2e501eea906edf94a5da1
WinkWink <amanda.winkler44@gmail.com>
2020-01-29 13:42:16 -0500

    questions

--- question.js
+++ question.js
@@ -1,32 +1,10 @@
-const readline = require("readline");
-
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-})
+const collectAnswers = require("./lib/collectAnswers");
 
 const questions = [
-	"what is your name? ",
-	"what is your favorite color ? ",
-	"why do you want to be a web developer? "
+"what is your name? ",
+"what is your favorite color ? ",
+"why do you want to be a developer? "
 ];
-
-const collectAnswers = (questions, done) => {
-	const answers = [];
-	const [firstQuestion] = questions;
-
-	const questionAnswered = answer => {
-		answers.push(answer);
-		if (answers.length < questions.length){
-			rl.question(questions[answers.length], questionAnswered);
-		}else{
-			done(answers);
-		}
-	}
-
-	rl.question(firstQuestion, questionAnswered);
-
-};
 
 collectAnswers(questions, answers => {
 	console.log("Thank you for your answers. ");
